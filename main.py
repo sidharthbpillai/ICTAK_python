a982bff98bdf17483ae10c73b64b578cf2c241a5
Sidhart B Pillai <sidharthbinu@gmail.com>
2024-07-11 13:00:25 +0530

    mesage to commit

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 from bank_operations.bank import credit, debit, transfer
 
 
+
+class Fruits:
+    def  __init__(self, name, color) -> None:
+        self.name = name
+        self.color = color
+
+    def say_color(self):
+        print("My color is", self.color)
+
+class Apple(Fruits):
+    def my_type(self):
+        print("I am an apple..")
+
+    def say_color(self):
+        print("I am red in color")  
+
+
+a = Apple("Apple1", "red")
+
+print(a.color)
